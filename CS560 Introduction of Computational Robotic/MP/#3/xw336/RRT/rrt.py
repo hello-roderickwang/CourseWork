f2892b145dbd7d44000e451bbca388f8779a5950
Roderick Wang <roderick_wang@outlook.com>
2019-12-02 00:10:29 -0500

    update cs510

---
+++ CS560 Introduction of Computational Robotic/MP/#3/xw336/RRT/rrt.py
@@ -0,0 +1,219 @@
+import sys
+
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+import numpy as np
+
+'''
+Set up matplotlib to create a plot with an empty square
+'''
+def setupPlot():
+    fig = plt.figure(num=None, figsize=(5, 5), dpi=120, facecolor='w', edgecolor='k')
+    ax = fig.subplots()
+    ax.set_axisbelow(True)
+    ax.set_ylim(-1, 11)
+    ax.set_xlim(-1, 11)
+    ax.grid(which='minor', linestyle=':', alpha=0.2)
+    ax.grid(which='major', linestyle=':', alpha=0.5)
+    return fig, ax
+
+'''
+Make a patch for a single pology 
+'''
+def createPolygonPatch(polygon, color):
+    verts = []
+    codes= []
+    for v in range(0, len(polygon)):
+        xy = polygon[v]
+        verts.append((xy[0], xy[1]))
+        if v == 0:
+            codes.append(Path.MOVETO)
+        else:
+            codes.append(Path.LINETO)
+    verts.append(verts[0])
+    codes.append(Path.CLOSEPOLY)
+    path = Path(verts, codes)
+    patch = patches.PathPatch(path, facecolor=color, lw=1)
+
+    return patch
+    
+
+'''
+Render the problem  
+'''
+def drawProblem(robotStart, robotGoal, polygons):
+    _, ax = setupPlot()
+    patch = createPolygonPatch(robotStart, 'green')
+    ax.add_patch(patch)    
+    patch = createPolygonPatch(robotGoal, 'red')
+    ax.add_patch(patch)    
+    for p in range(0, len(polygons)):
+        patch = createPolygonPatch(polygons[p], 'gray')
+        ax.add_patch(patch)    
+    plt.show()
+
+'''
+Grow a simple RRT 
+'''
+def growSimpleRRT(points):
+    newPoints = {}
+    adjListMap = {}
+    
+    # Your code goes here
+    
+    return newPoints, adjListMap
+
+'''
+Perform basic search 
+'''
+def basicSearch(tree, start, goal):
+    path = []
+    
+    # Your code goes here. As the result, the function should
+    # return a list of vertex labels, e.g.
+    #
+    # path = [23, 15, 9, ..., 37]
+    #
+    # in which 23 would be the label for the start and 37 the
+    # label for the goal.
+    
+    return path
+
+'''
+Display the RRT and Path
+'''
+def displayRRTandPath(points, adjListMap, path, robotStart=None, robotGoal=None, polygons=None):
+    # Your code goes here
+    # You could start by copying code from the function
+    # drawProblem and modify it to do what you need.
+    # You should draw the problem when applicable.
+    return
+
+'''
+Collision checking
+'''
+def isCollisionFree(robot, point, obstacles):
+
+    # Your code goes here.
+    
+    return False
+
+'''
+The full RRT algorithm
+'''
+def RRT(robot, obstacles, startPoint, goalPoint):
+
+    points = dict()
+    tree = dict()
+    path = []
+    # Your code goes here.
+    
+    return points, tree, path
+
+def main(filename, x1, y1, x2, y2, display=''):
+    # Read data and parse polygons
+    lines = [line.rstrip('\n') for line in open(filename)]
+    robot = []
+    obstacles = []
+    for line in range(0, len(lines)):
+        xys = lines[line].split(';')
+        polygon = []
+        for p in range(0, len(xys)):
+            xy = xys[p].split(',')
+            polygon.append((float(xy[0]), float(xy[1])))
+        if line == 0 :
+            robot = polygon
+        else:
+            obstacles.append(polygon)
+
+    # Print out the data
+    print("Robot:")
+    print(str(robot))
+    print("Pologonal obstacles:")
+    for p in range(0, len(obstacles)):
+        print(str(obstacles[p]))
+    print("")
+
+    # Visualize
+    if display == 'display':
+        robotStart = [(x + x1, y + y1) for x, y in robot]
+        robotGoal = [(x + x2, y + y2) for x, y in robot]
+        drawProblem(robotStart, robotGoal, obstacles)
+
+    # Example points for calling growSimpleRRT
+    # You should expect many mroe points, e.g., 200-500
+    points = dict()
+    points[1] = (5, 5)
+    points[2] = (7, 8.2)
+    points[3] = (6.5, 5.2)
+    points[4] = (0.3, 4)
+    points[5] = (6, 3.7)
+    points[6] = (9.7, 6.4)
+    points[7] = (4.4, 2.8)
+    points[8] = (9.1, 3.1)
+    points[9] = (8.1, 6.5)
+    points[10] = (0.7, 5.4)
+    points[11] = (5.1, 3.9)
+    points[12] = (2, 6)
+    points[13] = (0.5, 6.7)
+    points[14] = (8.3, 2.1)
+    points[15] = (7.7, 6.3)
+    points[16] = (7.9, 5)
+    points[17] = (4.8, 6.1)
+    points[18] = (3.2, 9.3)
+    points[19] = (7.3, 5.8)
+    points[20] = (9, 0.6)
+
+    # Printing the points
+    print("")
+    print("The input points are:")
+    print(str(points))
+    print("")
+    
+    points, adjListMap = growSimpleRRT(points)
+    print("")
+    print("The new points are:")
+    print(str(points))
+    print("")
+    print("")
+    print("The tree is:")
+    print(str(adjListMap))
+    print("")
+
+    # Search for a solution  
+    # change 1 and 20 as you want
+    path = basicSearch(adjListMap, 1, 2)
+    print("")
+    print("The path is:")
+    print(str(path))
+    print("")
+
+    # Your visualization code 
+    if display == 'display':
+        displayRRTandPath(points, adjListMap, path) 
+
+    # Solve a real RRT problem
+    points, adjListMap, path = RRT(robot, obstacles, (x1, y1), (x2, y2))
+    
+    # Your visualization code 
+    if display == 'display':
+        displayRRTandPath(points, adjListMap, path, robotStart, robotGoal, obstacles) 
+
+
+if __name__ == "__main__":
+    # Retrive file name for input data
+    if(len(sys.argv) < 6):
+        print("Five arguments required: python spr.py [env-file] [x1] [y1] [x2] [y2]")
+        exit()
+    
+    filename = sys.argv[1]
+    x1 = float(sys.argv[2])
+    y1 = float(sys.argv[3])
+    x2 = float(sys.argv[4])
+    y2 = float(sys.argv[5])
+    display = ''
+    if(len(sys.argv) == 7):
+        display = sys.argv[6]
+
+    main(filename, x1, y1, x2, y2, display)
